84cfd737abbd00064ab5c1751d67213ae11bca96
dongjoon <kok3443@naver.com>
2023-12-06 08:56:18 +0900

    2023.12.06 변수 정리

---
+++ src/variable/ex/VarEx2.java
@@ -0,0 +1,18 @@
+package variable.ex;
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        /**
+         * 변수 num1 을 선언하고 이에 10을 할당하세요
+         * 변수 num2 를 선언하고 이에 20을 할당하세요
+         * 두 변수의 합을구하고 그결과를 새로운 변수 sum에 저장하세요
+         */
+
+      int num1 = 10;
+      int num2 = 20;
+
+      int sum = num1 + num2;
+        System.out.println(sum);
+
+    }
+}
